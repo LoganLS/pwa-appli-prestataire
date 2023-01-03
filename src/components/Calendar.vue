20250e7f689c7eb85ac62abeefb237c6d2fc5fea
LoganLS <logan_56@hotmail.fr>
2023-01-03 17:40:40 +0100

    feat: start notification

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,7 +3,7 @@ import { onMounted, ref } from 'vue'
 import '@fullcalendar/core/vdom'
 import FullCalendar from "@fullcalendar/vue3"
 import dayGridPlugin from '@fullcalendar/daygrid'
-import { getRdvs, RDV } from '@/services/rdv';
+import { getRdvs, RDV, refreshRdv } from '@/services/rdv'
 
 interface event {
   title: string,
@@ -29,10 +29,60 @@ const calendarOptions = ref({
   firstDay: 1,
   events: events,
 })
+
+function registerNotification() {
+    Notification.requestPermission(permission => {
+    if (permission === 'granted') {
+      registerBackgroundSync();
+    } else console.error('Il faut les permissions pour recevoir les notifications');
+  })
+}
+
+function registerBackgroundSync() {
+  if (!navigator.serviceWorker) {
+    return console.error('Service Worker not supported');
+  }
+
+  navigator.serviceWorker.ready
+    .then(registration => {
+        registration.addEventListener('sync', function(event) {
+          console.log('sync event')
+          console.log(event)
+          if (event.tag === 'syncNotif') {
+              event.waitUntil(syncRdv()); // on lance la requête de synchronisation
+          }
+        });
+    })
+    .then(() => console.log('Registered background sync'))
+    .catch(err => console.error('Error registering background sync', err));
+}
+
+function syncRdv() {
+  console.log('refresh rdvs')
+    return getRdvs()
+    .then((response) => {
+      self.clients.matchAll().then(clients => {
+        clients.forEach(client => {
+          // signaler et envoyer au client les nouvelles données
+          client.postMessage(response);
+        });
+      });
+      return response // résout la promesse avec les nouvelles données
+    })
+    .then(rdvs => {
+        console.log(rdvs)
+        self.registration.showNotification(
+            `${rdvs.length} attendees to the PWA Workshop`
+        )
+    });
+}
 </script>
 
 <template>
   <section class="mt-5">
+    <VBtn @click="registerNotification()">
+      Recevoir les notifications
+    </VBtn>
     <FullCalendar :options="calendarOptions" />
   </section>
 </template>
